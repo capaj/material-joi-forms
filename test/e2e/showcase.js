375814561312417368d53c0d38030bed74ff7d8a
Jiří Špác <capajj@gmail.com>
2015-01-23 22:46:57 +0100

    basic types

--- test/e2e/showcase.js
+++ test/e2e/showcase.js
@@ -9,8 +9,14 @@ require('json-editor');
 require('angular-json-editor');
 require('angular-strap');
 require('jspm_packages/github/mgcrea/angular-strap@2.1.6/dist/angular-strap.tpl.min');
-require('joi');
+var Joi = require('joi');
 
-require('./js/module').controller('mainCtrl', function() {
-
+require('./js/module').controller('mainCtrl', function($scope) {
+	$scope.basicTypes = {
+		string: Joi.string(),
+		array: Joi.array(),
+		boolean: Joi.boolean(),
+		date: Joi.date(),
+		number: Joi.number()
+	}
 });
